cd9eefdf8b3517341464923aaab03c939042acfc
ronabaldoza <joddivicentemicabalo@gmail.com>
2025-02-12 10:59:49 +0800

    asd
    MY

---
+++ Palomera_IT13B_Activities/src/Prelim/Lab1_Arithmetics.java
@@ -0,0 +1,15 @@
+package Prelim;
+
+public class Lab1_Arithmetics {
+    public static void main(String [] args) {
+        
+        int x = 10;
+        int y = 4;
+        int z = 6;
+        
+        System.out.println(x + " * " + y " + " + z (x * y + z));
+
+        
+    }
+
+}
